4d21ef29e98e073c714dda8e3447d3bf95e24bb9
ABoccone <andreaboccone33@gmail.com>
2022-12-08 14:53:53 +0100

    metallurcig infused JSOn

--- Calculator.DAL/Models/JSONMekanismCT.cs
+++ Calculator.DAL/Models/JSONMekanismCT.cs
@@ -6,20 +6,48 @@ using System.Threading.Tasks;
 
 namespace Calculator.DAL.Models
 {
+    public class JSONRecipe
+    {
+        public string type { get; set; }
+    }
+
+
+
     public class Result
     {
         public string item { get; set; }
         public string tag { get; set; }
     }
 
+    public class ChemicalInput
+    {
+        public int amount { get; set; }
+        public string tag { get; set; }
+        public string  item { get; set; }
+    }
+
+    public class ItemInput
+    {
+        public int amount { get; set; }
+        public Result ingredient { get; set; }
+    }
+
     /// <summary>
     /// JSON Mekanism Crafting Table Repice
     /// </summary>
-    public class JSONMekanismCTR
+    public class JSONMekanismCTR : JSONRecipe
     {
-        public string type { get; set; }
         public Dictionary<string, Result> key { get; set; }
         public List<string> pattern { get; set; }
         public Result result { get; set; }
+    }
+
+    public class JSONMekanismMetallurgicInfusing : JSONRecipe
+    {
+        public ChemicalInput chemicalInput;
+
+        public ItemInput itemInput;
+        public List<string> pattern { get; set; }
+        public Result output { get; set; }
     }
 }
